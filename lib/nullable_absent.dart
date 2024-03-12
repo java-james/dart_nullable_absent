53b13bf3012f859a5417cd9541275479d6af6299
chalermpong <chalermpong@ltman.com>
2024-03-12 09:36:52 +0700

    Add documentation, add lint rule for documentation

--- lib/nullable_absent.dart
+++ lib/nullable_absent.dart
@@ -1,3 +1,4 @@
+/// Library provides a simple class that represents `absent` value
 library;
 
 export 'src/nullable_absent_base.dart';
--- lib/src/nullable_absent_base.dart
+++ lib/src/nullable_absent_base.dart
@@ -1,16 +1,22 @@
+
+/// Wrapper class for representing absent value.
 class NullableAbsent<T> {
   final bool _absent;
   final T? _value;
 
+  /// Create a present [value]
   const NullableAbsent(T? value):
         _absent = false,
         _value = value;
 
+  /// Create an absent
   const NullableAbsent.absent():
       _absent = true,
       _value = null;
 
+  /// Whether the value is absent
   bool get isAbsent => _absent;
 
+  /// Return the result value when applying [this] on [oldValue]
   T? replace({required T? oldValue}) => _absent ? oldValue : _value;
 }
